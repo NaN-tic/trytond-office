f0b92d2d1a8504706980b217108eea2c72191aba
Albert Cervera i Areny <albert@nan-tic.com>
2026-08-24 14:08:31 +0200

    Remove brainbow compatibility

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,15 +3,12 @@
 # the full copyright notices and license terms.
 from . import (
     test_scenario_attachment_export, test_scenario_configuration_menu,
-    test_scenario_document_migration, test_scenario_document_template,
-    test_scenario_legacy_module, test_scenario_text_attachment)
+    test_scenario_document_template, test_scenario_text_attachment)
 
 
 __all__ = [
     'test_scenario_attachment_export',
     'test_scenario_configuration_menu',
-    'test_scenario_document_migration',
     'test_scenario_document_template',
-    'test_scenario_legacy_module',
     'test_scenario_text_attachment',
     ]
